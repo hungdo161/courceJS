3287694d9d1b8695ccd46a7b58baa4dbec2ba9a9
hungdo161 <dongochung.161@gmail.com>
2023-07-17 23:33:39 +0700

    JS course 17/07/2023

--- main.js
+++ main.js
@@ -16,12 +16,16 @@ var today = new Date ();
         'Friday',
         'Saturday'
     ];
+
     var hour = today.getHours();
     var minute = today.getMinutes();
     var second = today. getSeconds();
 
     var prepand = (hour > 12)? " PM " : " AM "; // Prepand: chuẩn bị trước
-    hour = (hour >= 12) ? hour - 12 : hour;
+    hour = (hour >= 12) ? `0${hour - 12}` : hour;
+    minute = (minute < 10)? `0${minute}`:minute;
+    second = (second < 10)?`0${second}`:second;
+
     if(hour === 0 && prepand === ' PM ') {
         if (minute === 0 && second === 0) {
             hour = 12;
@@ -46,7 +50,48 @@ var today = new Date ();
     getTimeElemnt.innerHTML = `Current Time: ${prepand}${hour}:${minute}:${second}`;
 
 
-// Viết chương trình JavaScript để in nội dung cửa sổ hiện tại
+//Exercises 2:  Viết chương trình JavaScript để in nội dung cửa sổ hiện tại
+
+const printCurrentPage = function () {
+    window.print();
+}
+
+// Exercises 3: In ra số ngày tháng hiện tại theo format cho trước bằng Javascript;
+// format: mm-dd-year / mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+
+const getCurrentDateElement1 = document.querySelector('.current-date-format1');
+const getCurrentDateElement2 = document.querySelector('.current-date-format2')
+const getCurrentDateElement3 = document.querySelector('.current-date-format3')
+
+
+var currentYear = today.getFullYear();
+var currentMonth = today.getMonth() + 1;
+var currentDay = today.getDate();
+currentMonth = (currentMonth < 10) ? `0${currentMonth}`:`${currentMonth}`;
+currentDay = (currentDay < 10) ? `0${currentDay}`:`${currentDay}`;
+
+getCurrentDateElement1.innerHTML = `${currentDay}-${currentMonth}-${currentYear}`;
+getCurrentDateElement2.innerHTML = `${currentDay}/${currentMonth}/${currentYear}`;
+getCurrentDateElement3.innerHTML = `${currentYear}/${currentMonth}/${currentDay}`;
+
+// Exercises 4: Viết chương trình JavaScript để tìm diện tích của 1 tam giác có 3 cạnh là 5,6,7.
+
+const acreageResultElement = document.querySelector('.acreage-result');
+var side1 = 5;
+var side2 = 6;
+var side3 = 7;
+
+var primeter = (side1 + side2 + side3)/2; // 1 nửa của chu vi;
+var acreage = (Math.sqrt(primeter*(primeter-side1)*(primeter-side2)*(primeter-side3))).toFixed(3)
+
+acreageResultElement.innerHTML = `Result = : ${acreage}`;
+
+// Exercises 5: Viết chương trình javascript để xoay chuỗi 'W3resource' theo đúng hướng.
+// Note: Có thể thực hiện bằng cách loại bỏ 1 chữ cái khỏi đầu chuỗi và gắn nó vào phía trước.
+
+const runWord = 'W3resource' ;
+
+console.log(runWord)
 
 
     
